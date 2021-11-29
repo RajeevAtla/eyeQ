4d3f5b1b2d12dfaf99cf11429f7c6d64a18ec005
Rajeev Atla <rajeev@rajeevatla.com>
2021-11-29 08:29:07 -0500

    border

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 
 export default function index() {
+
+
     return (
 	<div className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-gray-800">
 	    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
@@ -12,10 +14,13 @@ export default function index() {
 		
 		
 		<div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-		    <a className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+		    <a className="p-6 mt-6 text-left w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
 			<h3 className="text-2xl dark:text-white font-bold">Upload &rarr;</h3>
 			<p className="mt-4 text-xl dark:text-white">
 			    Upload a file and have it read to you!
+			</p>
+			<p className="mt-4 text-xl dark:text-white">
+
 			</p>
 		    </a>
 		    
